6a7b091ad009d1c69942c4ef6466e88c5ecc12f4
unknown <seylaloeub@gmail.com>
2023-05-14 19:13:10 +0700

    create user

---
+++ sea-game-project/app/Http/Controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = User::all();
+        return response() -> json(['success' => true, 'data' => $user],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'name'=> request('name'),
+            'email'=> request('email'),
+            'password'=> request('password'),
+        ]);
+
+        return response() -> json(['Create success' => true, 'data' => $user],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::find($id);
+        return response() -> json(['success' => true, 'data' => $user],200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $user = User::find($id);
+
+        $user->update([
+            'name'=> $request->input('name'),
+            'email'=> $request->input('email'),
+            'password'=> $request->input('password'),
+        ]);
+
+        return response() -> json(['success' => true, 'data' => $user],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $users = User::find($id);
+        $users->delete();
+
+        return response() -> json(['success' => true, 'data' => $users],200);
+    }
+}
